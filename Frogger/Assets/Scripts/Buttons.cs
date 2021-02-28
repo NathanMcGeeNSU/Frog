d741662bad8998cd0d4fb70eacb8716170c046f3
Nathan McGee <nathanmcgee85@yahoo.com>
2021-02-27 23:29:26 -0600

    Frog

---
+++ Frogger/Assets/Scripts/Buttons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("QuitGame");
+    }
+}
---
+++ Frogger/Assets/Scripts/Car.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    
+
+    public float speed;
+
+    void Start ()
+    {
+        
+        Destroy(gameObject, 1);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 forward = new Vector2(transform.right.x, transform.right.y);
+        rb.MovePosition(rb.position + forward * Time.fixedDeltaTime * speed);
+    }
+
+    public void AdjustSpeed (float newSpeed)
+    {
+        speed = newSpeed;
+    }
+}
